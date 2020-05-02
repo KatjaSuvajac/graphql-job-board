242b5cd4c97a8802de7ccb091981fe824dfe4ca9
KatjaBraz <katja.braz@petrol.si>
2020-05-02 23:12:44 +0200

    lesson-20

--- client/src/request.js
+++ client/src/request.js
@@ -37,6 +37,10 @@ export async  function loadCompany(id) {
         id
         name
         description
+        jobs {
+          id
+          title
+        }
       }
     }`;
     const {company} = await graphQLRequest(query, {id});
