388be0de28223833fdf72c06d6bf95897a895e83
KatjaBraz <katja.braz@petrol.si>
2020-05-02 22:33:45 +0200

    lesson-17

--- client/src/request.js
+++ client/src/request.js
@@ -17,8 +17,29 @@ export async function loadJobs() {
             }`
         }),
     });
-
     const responseBody = await response.json();
-    console.log(responseBody);
     return responseBody.data.jobs;
+}
+
+export async function loadJob(id) {
+    const response = await fetch(endpoint, {
+        method: 'POST',
+        headers: {'content-type': 'application/json'},
+        body: JSON.stringify({
+            query: `query JobQuery($id: ID!) {
+              job(id: $id){
+                id
+                title
+                company {
+                  id
+                  name
+                }
+                description
+              }
+            }`,
+            variables: {id}
+        }),
+    });
+    const responseBody = await response.json();
+    return responseBody.data.job;
 }
